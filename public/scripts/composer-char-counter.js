20586bd6421b1f7a284138a0a1512b3f7f3a6f24
zahra-hm <zahra520@hotmail.com>
2020-12-16 23:19:09 +0000

    implement tweet character count

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
   // --- our code goes here ---
-});
+  $("#tweet-text").on('keyup', function() {
+    // console.log("ITS WORKING");
+    const tweetCount = $(this).val().length;
+    $("output").text(140-tweetCount);
+  })
+
+})
