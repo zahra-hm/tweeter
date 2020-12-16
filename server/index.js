702053016485513acbe9d612bba6cc20aac3e421
zahra-hm <zahra520@hotmail.com>
2020-12-16 22:12:27 +0000

    index javascript file

--- server/index.js
+++ server/index.js
@@ -23,14 +23,19 @@ const db = require("./lib/in-memory-db");
 const DataHelpers = require("./lib/data-helpers.js")(db);
 
 // Update the dates for the initial tweets (data-files/initial-tweets.json).
-require("./lib/date-adjust")();
+// require("./lib/date-adjust")();
 
 // The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
 // so it can define routes that use it to interact with the data layer.
 const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 
-// Mount the tweets routes at the "/tweets" path prefix:
+// Mount the tweets routes at the "/tweets" path prefix: 
 app.use("/tweets", tweetsRoutes);
+
+app.post("/tweets", function(req, res) {
+  res.json(tweetsRoutes);
+})
+
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
