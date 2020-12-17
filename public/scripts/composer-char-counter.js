b2d603bce08d8fa41c1f02d2161e3efacb9b247f
zahra-hm <zahra520@hotmail.com>
2020-12-17 07:27:51 +0000

    counts characters of tweet, count turns red when negative

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,8 +2,21 @@ $(document).ready(function() {
   // --- our code goes here ---
   $("#tweet-text").on('keyup', function() {
     // console.log("ITS WORKING");
-    const tweetCount = $(this).val().length;
+    var tweetCount = $(this).val().length;
     $("output").text(140-tweetCount);
+
+    // if (!tweetCount > 140) {
+    //   console.log("TOO MUCH");
+
+    // }
+
+    let $tc = $(this).closest('.input-box').siblings('.bottomline').find('.counter').text(140 - tweetCount);
+    if (tweetCount <= 140) {
+      $tc.removeClass('error');
+    } else {
+      $tc.addClass('error');
+    }
+
   })
 
-})
+});
