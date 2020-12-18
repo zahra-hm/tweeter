12b5ffb1ef8c7649b48866db6f258791b173032d
zahra-hm <zahra520@hotmail.com>
2020-12-18 09:20:52 +0000

    clean code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,22 +1,13 @@
 $(document).ready(function() {
   // --- our code goes here ---
   $("#tweet-text").on('keyup', function() {
-    // console.log("ITS WORKING");
     var tweetCount = $(this).val().length;
     $("output").text(140-tweetCount);
-
-    // if (!tweetCount > 140) {
-    //   console.log("TOO MUCH");
-
-    // }
-
     let $tc = $(this).closest('.input-box').siblings('.bottomline').find('.counter').text(140 - tweetCount);
     if (tweetCount <= 140) {
       $tc.removeClass('error');
     } else {
       $tc.addClass('error');
     }
-
   })
-
 });
